43cec3d7cf23073c845bc34b20c73eb15521f141
gaurav05-coder <gauravcoder09@gmail.com>
2025-04-19 22:12:35 +0530

    creating landing and authentication page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,88 +8,55 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    colors: {
-      border: 'hsl(var(--border))',
-      background: 'hsl(var(--background))',
-      foreground: 'hsl(var(--foreground))',
-      card: {
-        DEFAULT: 'hsl(var(--card))',
-        foreground: 'hsl(var(--card-foreground))'
-      },
-      popover: {
-        DEFAULT: 'hsl(var(--popover))',
-        foreground: 'hsl(var(--popover-foreground))'
-      },
-      primary: {
-        DEFAULT: 'hsl(var(--primary))',
-        foreground: 'hsl(var(--primary-foreground))'
-      },
-      secondary: {
-        DEFAULT: 'hsl(var(--secondary))',
-        foreground: 'hsl(var(--secondary-foreground))'
-      },
-      muted: {
-        DEFAULT: 'hsl(var(--muted))',
-        foreground: 'hsl(var(--muted-foreground))'
-      },
-      accent: {
-        DEFAULT: 'hsl(var(--accent))',
-        foreground: 'hsl(var(--accent-foreground))'
-      },
-      destructive: {
-        DEFAULT: 'hsl(var(--destructive))',
-        foreground: 'hsl(var(--destructive-foreground))'
-      },
-      input: 'hsl(var(--input))',
-      ring: 'hsl(var(--ring))',
-      chart: {
-        '1': 'hsl(var(--chart-1))',
-        '2': 'hsl(var(--chart-2))',
-        '3': 'hsl(var(--chart-3))',
-        '4': 'hsl(var(--chart-4))',
-        '5': 'hsl(var(--chart-5))'
-      }
-    },
-    borderColor: {
-      border: 'hsl(var(--border))',
-    },
-    extend: {
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
-      },
-      colors: {
-        card: {
-          foreground: 'hsl(var(--card-foreground))'
-        },
-        popover: {
-          foreground: 'hsl(var(--popover-foreground))'
-        },
-        primary: {
-          foreground: 'hsl(var(--primary-foreground))'
-        },
-        secondary: {
-          foreground: 'hsl(var(--secondary-foreground))'
-        },
-        muted: {
-          foreground: 'hsl(var(--muted-foreground))'
-        },
-        accent: {
-          foreground: 'hsl(var(--accent-foreground))'
-        },
-        destructive: {
-          foreground: 'hsl(var(--destructive-foreground))'
-        },
-        chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))'
-        }
-      }
-    }
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 };
